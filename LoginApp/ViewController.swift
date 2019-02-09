6b1cdcbedc5a189158d3e533b53937b07b717c2f
Safeen Azad <safeen_12@yahoo.com>
2019-02-10 00:52:09 +0300

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Safeen on 2/10/19.
+//  Copyright © 2019 Safeen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
